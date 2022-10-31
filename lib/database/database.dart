e476cb0a900b04475635010b4fc6f8e97d013d6c
Tomoutch <sinan.thomas@gmail.com>
2022-10-31 20:33:32 +0100

    Copie auto de la base de données

--- lib/database/database.dart
+++ lib/database/database.dart
@@ -1,19 +1,44 @@
 import 'dart:async';
+import 'dart:io';
 import 'package:flutter/widgets.dart';
+import 'package:logging/logging.dart';
 import 'package:path/path.dart';
 import 'package:sqflite/sqflite.dart';
+import 'package:flutter/services.dart' show ByteData, rootBundle;
+
+final log = Logger('Database');
 
 initDB() async {
 // Avoid errors caused by flutter upgrade.
 // Importing 'package:flutter/widgets.dart' is required.
   WidgetsFlutterBinding.ensureInitialized();
 // Open the database and store the reference.
-  final database = openDatabase(
-    // Set the path to the database. Note: Using the `join` function from the
-    // `path` package is best practice to ensure the path is correctly
-    // constructed for each platform.
-    join(await getDatabasesPath(), 'database.db'),
-  );
+  var databasesPath = await getDatabasesPath();
+  var path = join(databasesPath, "database.db");
 
-  return database;
+// Check if the database exists
+  var exists = await databaseExists(path);
+
+  if (!exists) {
+    // Should happen only the first time you launch your application
+    log.info("initDB() - Creating new copy from asset");
+
+    // Make sure the parent directory exists
+    try {
+      await Directory(dirname(path)).create(recursive: true);
+    } catch (_) {}
+
+    // Copy from asset
+    ByteData data = await rootBundle.load(join("assets", "database.db"));
+    List<int> bytes =
+    data.buffer.asUint8List(data.offsetInBytes, data.lengthInBytes);
+
+    // Write and flush the bytes written
+    await File(path).writeAsBytes(bytes, flush: true);
+
+  } else {
+    log.info("initDB() - Opening existing database");
+  }
+// open the database
+  return await openDatabase(path, readOnly: true);
 }
